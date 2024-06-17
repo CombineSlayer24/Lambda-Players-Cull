3f4a6a6fbe6b50cbfe606031d138c9eb79ef0ea7
CombineSlayer24 <82725572+CombineSlayer24@users.noreply.github.com>
2024-06-17 14:26:42 -0700

    Optimization
    Instead of using a think hook to constantly run, use a timer to periodically check and remove lambdaplayers.

--- lua/autorun/lambdaplayers_cull.lua
+++ lua/autorun/lambdaplayers_cull.lua
@@ -5,7 +5,7 @@
 -- or any game that that culls npcs, is to save performance
 -- Why have LambdaPlayers far from the player, outside their PVS
 -- and can't watch/witness their glory AI in motion, instead
--- putting stain and stress onto your not so Elon Musk SpaceX super 
+-- putting stain and stress onto your not so Elon Musk SpaceX super
 -- computer, to render a LambdaPlayer thats far, far away from you.
 -- Insteading of crying, let's cull our hateful LambdaPlayer that
 -- doesn't want anything to do with you, if it's not close to you
@@ -26,7 +26,7 @@ local function isLambdaPlayer(ent)
     return ent:IsNextBot() and ent:GetClass() == "npc_lambdaplayer"
 end
 
--- Check for the distance between the Players and Lamabda Players
+-- Check for the distance between the Players and Lambda Players
 local function isFarFromPlayer(ent, distance)
     for _, ply in pairs(player.GetAll()) do
         if ent:GetPos():DistToSqr( ply:GetPos() ) <= distance^2 then
@@ -37,31 +37,33 @@ local function isFarFromPlayer(ent, distance)
     return true
 end
 
--- Uh, if there's another way, instead of using a think hook,
--- I'm open to better ideas
-hook.Add("Think", "RemoveFarLambda", function()
-    if !isCullingEnabled:GetBool() then return end
+-- Function to remove Lambda Players that are far from any player
+local function RemoveFarLambda()
+    if not isCullingEnabled:GetBool() then return end
 
-    local lambdaPlayers = {}
+    local LambdaPlayerTable = {}
     local cullDistance = GetConVar( "lambdaplayers_cull_distance" ):GetInt()
 
     for _, ent in pairs(ents.GetAll()) do
         if isLambdaPlayer( ent ) then
-            lambdaPlayers[ #lambdaPlayers + 1 ] = ent
+            LambdaPlayerTable[ #LambdaPlayerTable + 1 ] = ent
         end
     end
 
-    if SERVER then -- Call this on ServerSide
-        for _, lambda in pairs( lambdaPlayers ) do -- Search for all Lambda Players.
+    if SERVER then
+        for _, lambda in pairs( LambdaPlayerTable ) do -- Search for all Lambda Players.
             if IsValid( lambda ) and isFarFromPlayer( lambda, cullDistance ) then
                 lambda:Remove() -- Remove them
             end
         end
     end
-end)
-	
+end
 
-CreateConVar("lambdaplayers_cull_distance", "3000", FCVAR_ARCHIVE, "If the LambdaPlayer's distance is far away from any player with the value set, the lambdaplayer will be culled.", 400, 10000)
+-- Set up a timer to periodically check and remove Lambda Players
+timer.Create("RemoveFarLambdaTimer", 5, 0, RemoveFarLambda)
+
+
+CreateConVar("lambdaplayers_cull_distance", "2000", FCVAR_ARCHIVE, "If the LambdaPlayer's distance is far away from any player with the value set, the lambdaplayer will be culled. 1600-2000, 2400-3000 recommended.", 400, 10000)
 CreateConVar("lambdaplayers_cull_enable", "1", FCVAR_ARCHIVE, "If LambdaPlayers should be culled.")
 
 if CLIENT then
@@ -78,8 +80,8 @@ if CLIENT then
             Panel:AddControl( "Button", { Label = "Reset to Defaults", Command = "lambdaplayers_cull_reset_defaults", Description = "Reset the culling distance to the default (recommended) values.", Text = "Reset to Defaults", Function = function() RunConsoleCommand("lambdaplayers_cull_distance", "3000") end } )
 
             Panel:CheckBox( "Enable Culling?","lambdaplayers_cull_enable" )
-            Panel:AddControl( "Slider", { Label = "Culling Distance", Command = "lambdaplayers_cull_distance", Min = "1000", Max = "10000" } )
-			Panel:ControlHelp( "If the distance between a Lambda Player and any player exceeds the value set, the Lambd aPlayer will be removed to improve performance." )
+            Panel:AddControl( "Slider", { Label = "Culling Distance", Command = "lambdaplayers_cull_distance", Min = "400", Max = "10000" } )
+			Panel:ControlHelp( "If the distance between a Lambda Player and any player exceeds the value set, the LambdaPlayer will be removed to improve performance.\n\nRecommended Values:\n\n 1500, 1600, 1800, 2000, 2400, 3000" )
 
         end)
     end)
@@ -87,5 +89,5 @@ if CLIENT then
     concommand.Add("lambdaplayers_cull_reset_defaults", function()
         RunConsoleCommand("lambdaplayers_cull_distance", "3000")
     end)
-    
+
 end
